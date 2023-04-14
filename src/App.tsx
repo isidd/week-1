ae7aa188df667d4bf6f2d6e6674b07bffee87afa
Siddhartha Pharasi <siddhartha.pharasi@nagarro.com>
2023-04-14 15:49:53 +0530

    week-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [item, setItem] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [bucketList, setBucketList] = useState<any>([]);
+
+  const addItemHandler = (e: any) => {
+    e.preventDefault();
+    let addedItem = {
+      id: (Math.random() * 1000).toFixed(0),
+      itemName: item,
+      quantity: quantity,
+    };
+    let currentItemList = bucketList;
+    currentItemList.push(addedItem);
+    setBucketList(currentItemList);
+    setItem('');
+    setQuantity('');
+  };
+
+  const removeListItem = (id: any) => {
+    let newBucketList = bucketList.filter((item: any) => item.id !== id);
+    setBucketList(newBucketList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="store">
+        <section>
+          <h1>Store</h1>
+        </section>
+        <form onSubmit={addItemHandler}>
+          <label htmlFor="item">Item</label>
+          <br />
+          <input type="text" id="item" name="item" onChange={(e) => setItem(e.target.value)} value={item}></input>{' '}
+          <br />
+          <label htmlFor="quantity">Quantity</label>
+          <br />
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            onChange={(e) => setQuantity(e.target.value)}
+            value={quantity}
+          ></input>{' '}
+          <br />
+          <input className="button" type="submit" value="Add Item"></input>
+        </form>
+      </section>
+      <section>
+        <section className="store">
+          <h1>Bucket</h1>
+        </section>
+        {bucketList.map((item: any) => (
+          <div onClick={() => removeListItem(item.id)} key={item.id} className="listItem">
+            {item.itemName} x {item.quantity}
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
